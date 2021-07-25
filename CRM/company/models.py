5ae3845515931feb4c694c00eaa1ef84db3d42b3
ANejadkoorki <amirh13811399>
2021-07-25 11:59:34 +0430

    WIP : Company app  models updated.

--- CRM/company/models.py
+++ CRM/company/models.py
@@ -1,3 +1,19 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from organization import models as organmodels
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class CompanyProduct(models.Model):
+    """
+        represents a product object from our own company
+    """
+    product_name = models.CharField(max_length=50, verbose_name=_('product name'))
+    price = models.PositiveIntegerField(default=0, verbose_name=_('price'))
+    have_tax = models.BooleanField(default=False)
+    pdf_catalog = models.FileField(validators=[FileExtensionValidator(allowed_extensions=['pdf'])],
+                                   verbose_name=_('pdf catalog'))
+    image_catalog = models.ImageField(verbose_name=_('image catalog'), blank=True, null=True)
+    technical_desc = models.TextField(verbose_name=_('technical description'))
+    usable_for_organizations_product = models.ManyToManyField(organmodels.OrganizationsProduct,
+                                                              verbose_name=_('usable for organizations product'))
