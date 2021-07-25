be3f18e17abd07d848d16424e8b81566d3673a9b
ANejadkoorki <amirh13811399>
2021-07-25 11:33:35 +0430

    WIP : starting Project and creating apps.

---
+++ CRM/organization/models.py
@@ -0,0 +1,34 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django_jalali.db import models as jmodels
+from django.utils.translation import ugettext_lazy as _
+
+# here is the regex validators for iranian Landline and phone number.
+landline_phone_regex = RegexValidator(regex='^0[0-9]{2,}[0-9]{7,}$', message=_('This Landline number is Invalid.'))
+Phone_number_regex = RegexValidator(regex='09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}',
+                                    message=_('This Phone number is Invalid.'))
+
+
+class OrganizationsProduct(models.Model):
+    """
+        represents an product that manufactured in organizations
+    """
+    name = models.CharField(max_length=50, verbose_name=_('Product name'))
+
+
+class Organization(models.Model):
+    """
+        represents an organization object contains organization`s characteristics
+    """
+    province = models.CharField(max_length=20, verbose_name=_('Province'))
+    organization_name = models.CharField(max_length=50, verbose_name=_('Organization name'))
+    telephone = models.CharField(validators=[landline_phone_regex], max_length=11,
+                                 verbose_name=_('Organization telephone'))
+    workers_qty = models.PositiveIntegerField(default=1, verbose_name=_('Workers Quantity'))
+    manufactured_product = models.ManyToManyField(OrganizationsProduct, verbose_name=_('Manufactured product'))
+    representative_full_name = models.CharField(max_length=80, verbose_name=_('Representative fullname'))
+    representative_phone_number = models.CharField(validators=[Phone_number_regex], max_length=11,
+                                                   verbose_name=_('Representative phone number'))
+    representative_email = models.EmailField(verbose_name=_('Representative email'))
+    created_on = jmodels.jDateTimeField(auto_now_add=True, verbose_name=_('Creation time'))
+    expert = models.ForeignKey('auth.User', verbose_name=_('Expert'), on_delete=models.PROTECT)
