02f599bf9c3d1e7ee1f6f8c8ff3953fb43ef2ba1
ANejadkoorki <amirh13811399>
2021-08-06 11:54:12 +0430

    WIP : Project Phase2 Completed(commenting).

--- CRM/emailApp/admin.py
+++ CRM/emailApp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# admin site header
+admin.site.site_header = 'CRM System'
+
+
+@admin.register(models.EmailHistory)
+class EmailHistoryAdmin(admin.ModelAdmin):
+    """
+        represents EmailHistory Model admin interface
+    """
+    list_display = [
+        'id',
+        'created_on',
+        'sender',
+        'receiver_email_address',
+        'is_successful',
+    ]
